def2b95729fb01b5325eaa625c86c2d49086ff94
Kyle Warren <78629937+basedinbham@users.noreply.github.com>
2022-06-19 18:11:00 -0700

    added FlagImage struct

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -47,10 +47,7 @@ struct ContentView: View {
                         Button {
                             flagTapped(number)
                         } label: {
-                            Image(countries[number])
-                                .renderingMode(.original)
-                                .clipShape(Capsule())
-                                .shadow(radius: 5)
+                            FlagImage(name: countries[number])
                         }
                     }
                 }
